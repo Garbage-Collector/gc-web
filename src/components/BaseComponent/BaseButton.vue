62bc7fd0c58408d1815ca298e10fdc22ffd6a256
junoshon <sanoldfox@naver.com>
2024-04-13 13:20:25 +0900

    feat: BaseComponent 추가

---
+++ src/components/BaseComponent/BaseButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-btn :style="buttonStyle" :label="text"/>
+</template>
+
+<script setup lang="ts">
+import {QBtn} from 'quasar';
+import {computed} from "vue";
+
+type ButtonSize = 'sm' | 'md' | 'lg';
+
+interface BaseButtonProps {
+  size: ButtonSize;
+  color: string;
+  text: string;
+}
+
+const props = defineProps<BaseButtonProps>();
+const buttonStyle = computed(() => {
+  const sizeStyles = {
+    sm: {width: '76px', height: '34px', color: 'white', borderRadius: '16px'},
+    //TODO : md, lg 사이즈 추후 추가 예정
+    md: {width: '200px', height: '60px', color: 'white'},
+    lg: {width: '300px', height: '90px', color: 'white'},
+  };
+
+  // props에서 size와 color를 추출합니다.
+  const {size, color} = props;
+
+  // 현재 size에 맞는 스타일을 가져옵니다.
+  const currentSizeStyle = sizeStyles[size];
+
+  // 계산된 스타일 객체를 반환합니다.
+  return {
+    ...currentSizeStyle,
+    backgroundColor: color, // props의 color 값을 배경색으로 사용합니다.
+  };
+});
+</script>
---
+++ src/components/BaseComponent/BaseCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="base-card">
+    <img :src="photo" :alt="title" class="card-image" />
+
+    <article class="content-wrapper">
+      <span class="title">{{ title }}</span>
+      <p class="content">{{ content }}</p>
+    </article>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+// 정의된 props
+interface Props {
+  photo: string;
+  title: string;
+  content: string;
+}
+defineProps<Props>();
+</script>
+
+<style scoped>
+.base-card {
+  border: 1px solid #ccc;
+  border-radius: 16px;
+  width: 193px;
+  height: 203px;
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.3);
+  border: none;
+}
+
+.card-image {
+  width: 100%;
+  height: 65%;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+}
+.title {
+  color: #2c2c2c;
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.content {
+  color: #7b7b7b;
+  font-size: 12px;
+  font-weight: 500;
+}
+.content-wrapper {
+  padding: 8px 16px 8px 8px;
+}
+</style>
