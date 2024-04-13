13dcc21d0dbe7e7a09d36f63f92cdd761ac925fa
junoshon <sanoldfox@naver.com>
2024-04-13 15:35:12 +0900

    feat: input, button 공통 컴포넌트 수정

--- src/components/BaseComponent/BaseButton.vue
+++ src/components/BaseComponent/BaseButton.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 import {QBtn} from 'quasar';
-import {computed} from "vue";
+import {computed} from 'vue';
 
 type ButtonSize = 'sm' | 'md' | 'lg';
 
@@ -23,16 +23,16 @@ const buttonStyle = computed(() => {
     lg: {width: '300px', height: '90px', color: 'white'},
   };
 
-  // props에서 size와 color를 추출합니다.
+  // props에서 size와 color를 추출
   const {size, color} = props;
 
-  // 현재 size에 맞는 스타일을 가져옵니다.
+  // 현재 size에 맞는 스타일을 가져옴
   const currentSizeStyle = sizeStyles[size];
 
-  // 계산된 스타일 객체를 반환합니다.
+  // 계산된 스타일 객체를 반환
   return {
     ...currentSizeStyle,
-    backgroundColor: color, // props의 color 값을 배경색으로 사용합니다.
+    backgroundColor: color, // TODO: bg-color, color 로 구분
   };
 });
 </script>
--- src/components/BaseComponent/BaseInput.vue
+++ src/components/BaseComponent/BaseInput.vue
@@ -1,13 +1,60 @@
-<template>
-  <div>
+<!--<template>-->
+<!--  <div class="base-input">-->
+<!--    <q-input-->
+<!--      :label="computedLabel"-->
+<!--      :placeholder="placeholder"-->
+<!--      :rules="inputRules"-->
+<!--      bordered-->
+<!--      :model-value="modelValue"-->
+<!--      @update:model-value="updateValue"-->
+<!--    />-->
+<!--  </div>-->
+<!--</template>-->
 
-  </div>
-</template>
+<!--<script setup lang="ts">-->
+<!--import { computed } from 'vue';-->
+<!--import { QInput } from 'quasar';-->
 
-<script setup lang="ts">
+<!--interface BaseInputProps {-->
+<!--  label: string;-->
+<!--  placeholder?: string;-->
+<!--  required?: boolean;-->
+<!--  modelValue: string;-->
+<!--}-->
 
-</script>
+<!--const props = defineProps<BaseInputProps>();-->
+<!--// const emits = defineEmits(['update:modelValue']);  // 이벤트를 정의-->
 
-<style scoped>
 
-</style>
+<!--const inputRules = computed(() => {-->
+<!--  if (props.required) {-->
+<!--    return [-->
+<!--      (val: string | null) => (val && val.length > 0) || 'This field is required',-->
+<!--    ];-->
+<!--  }-->
+<!--  return [];-->
+<!--});-->
+
+<!--const computedLabel = computed(() => {-->
+<!--  return props.required ? `${props.label} *` : props.label;-->
+<!--});-->
+
+<!--function updateValue(value: string | number | null): void {-->
+<!--  // value가 null이나 number일 수 있으므로, 처리 로직에 이에 대한 처리가 필요할 수 있음-->
+<!--  if (value === null) {-->
+<!--    // null일 때의 처리-->
+<!--    console.log("Value is null");-->
+<!--  } else {-->
+<!--    // string 또는 number일 때의 처리-->
+<!--    console.log("Value is:", value);-->
+<!--  }-->
+<!--}-->
+
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--.base-input .q-field__label.required::after {-->
+<!--  content: '*';-->
+<!--  color: red;-->
+<!--}-->
+<!--</style>-->
