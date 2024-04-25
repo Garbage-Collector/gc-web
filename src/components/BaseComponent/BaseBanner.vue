3ed304da1eaa5b76fc9b4aad284537e638ada433
junoshon <sanoldfox@naver.com>
2024-04-25 23:59:01 +0900

    feat: 공통 Banner 컴포넌트 구현

---
+++ src/components/BaseComponent/BaseBanner.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="banner-wrapper">
+    <!-- <img :src="props.imgSrc" :alt="title" class="banner-image" /> -->
+    <img src="../../assets/피드이미지.png" alt="" />
+    <div :id="id" class="banner">
+      <div class="banner-info">
+        <p class="title">{{ props.title }}</p>
+        <span class="date"> {{ props.startDate }}</span>
+
+        <div class="icon-wrapper">
+          <BaseIcon icon="like" />
+          <BaseIcon icon="share" />
+          <BaseIcon icon="burgerMenu" />
+          <BaseIcon icon="trashCan" />
+        </div>
+        <q-btn
+          round
+          color="green"
+          icon="edit"
+          style="width: 30px; height: 30px"
+          class="edit-button"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import BaseIcon from 'src/components/BaseComponent/BaseIcon.vue';
+type Props = {
+  id: string;
+  title: string;
+  startDate: string;
+  imgSrc: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.banner-wrapper {
+  display: flex;
+  height: 100px;
+}
+.banner {
+  background: rgb(165, 211, 181);
+  filter: brightness(108%);
+  border-radius: 0px 10px 10px 0px;
+  width: 259px;
+  height: 100px;
+  position: relative;
+  padding: 15px 15px;
+}
+.banner-image {
+  width: 100px;
+  height: 100%;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.title {
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 0;
+}
+.date {
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+}
+.banner-info {
+  display: flex;
+  flex-direction: column;
+}
+.icon-wrapper {
+  margin-top: 16px;
+  display: flex;
+  gap: 8px;
+}
+.edit-button {
+  position: absolute;
+  top: 30px;
+  right: 20px;
+  z-index: 10;
+}
+</style>
--- src/pages/FeedPage.vue
+++ src/pages/FeedPage.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    FeedPage
+    <BaseIcon icon="like" />
+    <BaseIcon icon="share" />
+    <BaseBanner
+      id="1"
+      title="홍천 비발디파크 플로깅"
+      start-date="2024/01/12"
+      imgSrc="../../assets/피드이미지.png"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import BaseIcon from 'src/components/BaseComponent/BaseIcon.vue';
+import BaseBanner from 'src/components/BaseComponent/BaseBanner.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
