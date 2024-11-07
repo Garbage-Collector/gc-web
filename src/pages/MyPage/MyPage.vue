3efdcd3d5b0cd6946e9dbfc3398aa314a0c843b4
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-11-07 20:24:04 +0900

    merge dongmin branch

--- src/pages/MyPage/MyPage.vue
+++ src/pages/MyPage/MyPage.vue
@@ -139,7 +139,7 @@ const passkeyRegister = async () => {
     const email = profileStore.profile.email;
 
     const resp = await fetch(
-      `http://localhost:8080/api/passkey/register?email=${encodeURIComponent(email)}`,
+      `${import.meta.env.VITE_BASE_URL}/passkey/register?email=${encodeURIComponent(email)}`,
     );
     const optionsJSON = await resp.json();
 
@@ -163,7 +163,7 @@ const passkeyRegister = async () => {
     // POST the response to the endpoint that calls
     // @simplewebauthn/server -> verifyRegistrationResponse()
     const verificationResp = await fetch(
-      'http://localhost:8080/api/passkey/register',
+      `${import.meta.env.VITE_BASE_URL}/passkey/register`,
       {
         method: 'POST',
         headers: {
